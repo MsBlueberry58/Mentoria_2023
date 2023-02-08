1237d7f98ea88c30b5329520df18b6f0c4093772
Bruna <bruna.guedes@ifood.com.br>
2023-02-08 10:16:05 -0300

    Criando docker-compose, mongo configs e afins

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,13 @@ plugins {
 	id("io.spring.dependency-management") version "1.1.0"
 	kotlin("jvm") version "1.7.22"
 	kotlin("plugin.spring") version "1.7.22"
+	id("idea")
+}
+
+idea {
+	module {
+		isDownloadSources = true
+	}
 }
 
 group = "com.bruna.guedes"
