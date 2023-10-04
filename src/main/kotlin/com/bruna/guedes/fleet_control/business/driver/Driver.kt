a937d33b526871e180b33b2fe80f59880b7959e3
bruna.guedes <bruna.guedes@ifood.com.br>
2023-10-04 10:53:37 -0300

    Adicionando constraint de CPF pra drivers Próximo passo - tratar exception pra negócio

--- src/main/kotlin/com/bruna/guedes/fleet_control/business/driver/Driver.kt
+++ src/main/kotlin/com/bruna/guedes/fleet_control/business/driver/Driver.kt
@@ -1,10 +1,14 @@
 package com.bruna.guedes.fleet_control.business.driver
 
 import org.springframework.data.annotation.Id
+import org.springframework.data.mongodb.core.index.Indexed
+import org.springframework.data.mongodb.core.mapping.Document
 import java.util.*
 
+@Document
 data class Driver(
     @Id val id: UUID,
     val name: String,
-    val cnhType: String
+    val cnhType: String,
+    @Indexed(unique = true) val cpf: String
 )
--- src/main/kotlin/com/bruna/guedes/fleet_control/business/driver/DriverDTO.kt
+++ src/main/kotlin/com/bruna/guedes/fleet_control/business/driver/DriverDTO.kt
@@ -4,15 +4,17 @@ import java.util.*
 
 data class NewDriverDTO(
     val name: String,
-    val cnhType: String
+    val cnhType: String,
+    val cpf: String
 )
 
 data class DriverDTO(
     val id: UUID,
     val name: String,
-    val cnhType: String
+    val cnhType: String,
+    val cpf: String
 )
 
-fun NewDriverDTO.toDriver() = Driver(UUID.randomUUID(), this.name, this.cnhType)
+fun NewDriverDTO.toDriver() = Driver(UUID.randomUUID(), this.name, this.cnhType, this.cpf)
 
-fun Driver.toDTO() = DriverDTO(this.id, this.name, this.cnhType)
+fun Driver.toDTO() = DriverDTO(this.id, this.name, this.cnhType, this.cpf)
